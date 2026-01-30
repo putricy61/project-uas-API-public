9de8675d59bbc585b37fdaf06ccd34eae8e994a5
putricy61 <126140392+putricy61@users.noreply.github.com>
2026-01-30 17:46:47 +0700

    Update air-quality-route.js

--- routes/air-quality-route.js
+++ routes/air-quality-route.js
@@ -1,15 +1,26 @@
-const express = require('express')
-const controller = require('../controllers/air-quality-controller')
-const authMiddleware = require('../middlewares/auth-middleware')
-const router = express.Router()
+// const express = require('express')
+// const controller = require('../controllers/air-quality-controller')
+// const authMiddleware = require('../middlewares/auth-middleware')
+// const router = express.Router()
 
-router.get('/air-quality', controller.getAirQuality);
+// router.get('/air-quality', controller.getAirQuality);
 
-router.post('/air-quality', authMiddleware, controller.saveAirQuality)
-router.get('/air-quality/location', authMiddleware, controller.getAirQualityByLocation)
+// router.post('/air-quality', authMiddleware, controller.saveAirQuality)
+// router.get('/air-quality/location', authMiddleware, controller.getAirQualityByLocation)
 
-router.delete('/air-quality/:id', authMiddleware, controller.deleteAirQuality)
+// router.delete('/air-quality/:id', authMiddleware, controller.deleteAirQuality)
 
 
 
-module.exports = router
+// module.exports = router
+const express = require('express');
+const router = express.Router();
+const airQualityController = require('../controllers/air-quality-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
+
+router.get('/search', authMiddleware, airQualityController.searchAndShow);
+router.post('/favorite', authMiddleware, airQualityController.saveToFavorite);
+router.get('/favorites', authMiddleware, airQualityController.getHistory);
+router.delete('/favorite/:id', authMiddleware, airQualityController.remove);
+
+module.exports = router;
