f38059919e2e07e435c1a2ff72d275f4e0fb10f1
putricy61 <126140392+putricy61@users.noreply.github.com>
2026-01-30 17:45:31 +0700

    Update weather-route.js

--- routes/weather-route.js
+++ routes/weather-route.js
@@ -1,14 +1,26 @@
+// const express = require('express');
+// const router = express.Router();
+// const controller = require('../controllers/weather-controller');
+// const authMiddleware = require('../middlewares/auth-middleware'); // Pastikan path foldernya benar
+
+// router.get('/weather', controller.getWeatherByCity);
+// router.post('/weather/sync', authMiddleware, controller.syncWeather);
+// router.get('/history', authMiddleware, controller.getAllWeather);
+// router.get('/latest/:city', authMiddleware, controller.getLatestWeather);
+// router.delete('/weather/city/:city', authMiddleware, controller.removeWeatherByCity);
+
+
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/weather-controller');
-const authMiddleware = require('../middlewares/auth-middleware'); // Pastikan path foldernya benar
+const weatherController = require('../controllers/weather-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
 
-router.get('/weather', controller.getWeatherByCity);
-router.post('/weather/sync', authMiddleware, controller.syncWeather);
-router.get('/history', authMiddleware, controller.getAllWeather);
-router.get('/latest/:city', authMiddleware, controller.getLatestWeather);
-router.delete('/weather/city/:city', authMiddleware, controller.removeWeatherByCity);
-
-
+router.get('/search', authMiddleware, weatherController.searchAndShow);
+router.post('/favorite', authMiddleware, weatherController.saveToFavorite);
+router.get('/favorites', authMiddleware, weatherController.getHistory);
+router.delete('/favorite/:id', authMiddleware, weatherController.remove);
+router.get('/stats', authMiddleware, weatherController.getStats);
 
 module.exports = router;
