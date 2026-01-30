8b6e506bfbacaf12bd37f9e46d43c42ca4121c6d
putricy61 <126140392+putricy61@users.noreply.github.com>
2026-01-30 17:46:15 +0700

    Update location-route.js

--- routes/location-route.js
+++ routes/location-route.js
@@ -1,12 +1,27 @@
-const express = require('express')
-const controller = require('../controllers/location-controller')
-const authMiddleware = require('../middlewares/auth-middleware')
-const router = express.Router()
+// const express = require('express')
+// const controller = require('../controllers/location-controller')
+// const authMiddleware = require('../middlewares/auth-middleware')
+// const router = express.Router()
 
-router.get('/location/search', controller.getLocation)
-router.post('/locations', authMiddleware, controller.saveLocation)
-router.get('/location/user', authMiddleware, controller.getUserLocation)
-router.get('/location/detail/:city', authMiddleware, controller.getLocationDetail)
-router.delete('/location/:city', authMiddleware, controller.deleteUserLocation)
+// router.get('/location/search', controller.getLocation)
+// router.post('/locations', authMiddleware, controller.saveLocation)
+// router.get('/location/user', authMiddleware, controller.getUserLocation)
+// router.get('/location/detail/:city', authMiddleware, controller.getLocationDetail)
+// router.delete('/location/:city', authMiddleware, controller.deleteUserLocation)
 
-module.exports = router
+// module.exports = router
+
+const express = require('express');
+const router = express.Router();
+const articleController = require('../controllers/article-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
+
+// Tampilan Default (API)
+router.get('/all', authMiddleware, articleController.getAllArticles);
+router.post('/search', authMiddleware, articleController.searchByQuery);
+router.post('/favorite', authMiddleware, articleController.saveToFavorite);
+router.get('/favorites', authMiddleware, articleController.getDbFavorites);
+router.delete('/favorite/:id', authMiddleware, articleController.remove);
+router.get('/favorites/:id', authMiddleware, articleController.getDetail);
+
+module.exports = router;
