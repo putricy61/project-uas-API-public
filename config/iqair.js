9ada1a2f04f9df6b130926637bde745213b9274c
putricy61 <126140392+putricy61@users.noreply.github.com>
2026-01-30 17:52:46 +0700

    Update iqair.js

--- config/iqair.js
+++ config/iqair.js
@@ -9,3 +9,18 @@ const iqair = axios.create({
 })
 
 module.exports = iqair
+
+
+newsapi
+
+require('dotenv').config()
+const axios = require('axios')
+
+const newsapi = axios.create({
+    baseURL: 'https://newsapi.org/v2',
+    params: {
+        apiKey: process.env.NEWS_API_KEY // Pastikan di .env namanya NEWS_API_KEY
+    }
+})
+
+module.exports = newsapi
